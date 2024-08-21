d0a2fed5eceaff54fbe5e4187048b6fbb7883119
Nikita Kyteyko <nikitakuteyko@gmail.com>
2024-08-21 14:39:54 +0300

    Implement files generation

--- DataFlow/DataFlow/ViewModel/FilesViewModel.cs
+++ DataFlow/DataFlow/ViewModel/FilesViewModel.cs
@@ -1,17 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+﻿using DataFlow.Interfaces;
+using DataFlow.Model;
+using DataFlow.Services;
+using DataFlow.Utilities;
+using DataFlow.View.ModalPages;
+using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
 
 namespace DataFlow.ViewModel
 {
     public class FilesViewModel : ViewModelBase
     {
+        Stack<string> navigationHistory = new Stack<string>();
+        readonly string PATH = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string buttonVisibility = "Visible";
+        string listVisibility = "Collapsed";
+        string currentDirectory;
+
+        public RelayCommand ChangeFolderCommand { get; set; }
+        public RelayCommand ReturnBackCommand { get; set; }
+        public RelayCommand ChooseFolderCommand { get; set; }
+        public RelayCommand OpenGenerateModalCommand { get; set; }
+        public RelayCommand RefreshCommand { get; set; }
+
+        public string CurrentDirectory
+        {
+            get { return currentDirectory; }
+            set { currentDirectory = value; OnPropertyChanged("CurrentDirectory"); navigationHistory.Push(currentDirectory); LoadContent(); }
+        }
+        public string ButtonVisibility
+        {
+            get { return buttonVisibility; }
+            set { buttonVisibility = value; OnPropertyChanged("ButtonVisibility"); }
+        }
+
+        public string ListVisibility
+        {
+            get { return listVisibility; }
+            set { listVisibility = value; OnPropertyChanged("ListVisibility"); }
+        }
+        public ObservableCollection<FileManagerItem> FolderContent{ get; set; }
+
         public FilesViewModel()
         {
-            
+            ChangeFolderCommand = new RelayCommand(param => ChangeFolder((FileManagerItem)param)) ;
+            ReturnBackCommand = new RelayCommand(param => ReturnBack());
+            ChooseFolderCommand = new RelayCommand(param => ChooseFolder());
+            OpenGenerateModalCommand = new RelayCommand(param => OpenGenerateModal());
+            RefreshCommand = new RelayCommand(param => LoadContent());
+            FolderContent = new ObservableCollection<FileManagerItem>();
+            LoadContent();
         }
+
+        private void LoadContent()
+        {
+            FolderContent.Clear();
+            try
+            {
+                if (Directory.Exists(CurrentDirectory))
+                {
+                    string fileIcon = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Resources", "File.png");
+                    string folderIcon = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Resources", "Folder.png");
+
+                    foreach (var dir in Directory.GetDirectories(CurrentDirectory))
+                    {
+                        FolderContent.Add(new FileManagerItem { Name = Path.GetFileName(dir), Path = dir, IsDirectory = true, Icon = folderIcon });
+                    }
+
+                    foreach (var file in Directory.GetFiles(CurrentDirectory))
+                    {
+                        FolderContent.Add(new FileManagerItem { Name = Path.GetFileName(file), Path = file, IsDirectory = false, Icon = fileIcon });
+                    }
+
+                    ButtonVisibility = "Collapsed";
+                    ListVisibility = "Visible";
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                ReturnBack();
+            }
+            catch(Exception ex) {
+                MessageBox.Show("Unexpected exception", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void ChangeFolder(FileManagerItem item)
+        {
+            if (item.IsDirectory)
+            { 
+                CurrentDirectory = item.Path;
+            }
+        }
+
+        private void ReturnBack()
+        {
+            
+            if (navigationHistory.Count > 1)
+            {
+                navigationHistory.Pop();
+                CurrentDirectory = navigationHistory.Pop(); 
+            }
+        }
+
+        private void ChooseFolder()
+        {
+            var folderDialog = new OpenFolderDialog();
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                navigationHistory.Clear();
+                var folderName = folderDialog.FolderName;
+                CurrentDirectory = folderName;
+            }
+        }
+
+        private void OpenGenerateModal()
+        {
+            new GenerateModalPage(CurrentDirectory).ShowDialog();
+        }
+
+      
     }
 }
---
+++ DataFlow/DataFlow/ViewModel/GenerateViewModel.cs
@@ -0,0 +1,75 @@
+﻿using DataFlow.Interfaces;
+using DataFlow.Services;
+using DataFlow.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DataFlow.ViewModel
+{
+    public class GenerateViewModel : ViewModelBase
+    {
+        string filesPrefix = "Generated_File";
+        string filesAmount = "100";
+        string rowsAmount = "100000";
+        string currentDirectory;
+        IFilesService filesService;
+        public string FilesPrefix
+        {
+            get { return filesPrefix; }
+            set { filesPrefix = value; OnPropertyChanged("FilesPrefix"); }
+        }
+        public string FilesAmount
+        {
+            get { return filesAmount; }
+            set { filesAmount = value; OnPropertyChanged("FilesAmount"); }
+        }
+        public string RowsAmount
+        {
+            get { return rowsAmount; }
+            set { rowsAmount = value; OnPropertyChanged("RowsAmount"); }
+        }
+
+        public RelayCommand CloseWindowCommand { get; set; }
+        public RelayCommand DragMoveWindowCommand { get; set; }
+        public RelayCommand GenerateStringCommand { get; set; }
+
+        public GenerateViewModel(string currentDirectory)
+        {
+            CloseWindowCommand = new RelayCommand(param => CloseWindow(param));
+            DragMoveWindowCommand = new RelayCommand(param => DragMoveWindow(param));
+            GenerateStringCommand = new RelayCommand(async param => await GenerateString());
+            this.currentDirectory = currentDirectory;
+            filesService = new FilesService(currentDirectory);
+        }
+
+        private void CloseWindow(object windowParam)
+        {
+            if(windowParam is Window window)
+            {
+                window.Close();
+            }
+        }
+
+        private void DragMoveWindow(object windowParam)
+        {
+            if (windowParam is Window window)
+            {
+                window.DragMove();
+            }
+        }
+
+        private async Task GenerateString()
+        {
+            if (currentDirectory is not null && FilesPrefix is not null && int.TryParse(FilesAmount,out var filesAmount) && int.TryParse(RowsAmount, out var rowsAmount))
+                await filesService.GenerateFiles(currentDirectory, FilesPrefix, filesAmount, rowsAmount);
+            else if (currentDirectory is null)
+                MessageBox.Show("Please  choose folder");
+            else
+                MessageBox.Show("Please enter correct data");
+        }
+    }
+}
